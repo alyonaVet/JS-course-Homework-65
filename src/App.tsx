f507465b35cd3733419746ec10211d732fb4b5d4
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-05 12:26:50 +0600

    added Routes to App container;

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-const App = () => {
+import Toolbar from './components/Toolbar/Toolbar';
+import {Route, Routes} from 'react-router-dom';
+import MainPage from './containers/MainPage/MainPage';
+import EditPage from './containers/EditPage/EditPage';
 
-    return (
-        <>
-            Hello, world!
-        </>
-    )
+const App = () => {
+  return (
+    <>
+      <header>
+        <Toolbar/>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<MainPage/>}/>
+          <Route path="/pages/:pageId" element={<MainPage/>}/>
+          <Route path="/pages/admin" element={<EditPage/>}/>
+          <Route path="*" element={<h3 className="mt-3 mb-5 text-center">Page not found</h3>}/>
+        </Routes>
+      </main>
+    </>
+  );
 };
 
 export default App;
