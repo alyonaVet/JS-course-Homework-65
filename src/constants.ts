6e6d932cf54add5022e32f42584367ea7f5eabce
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-05 11:33:39 +0600

    created constants file; added PAGES array;

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+import {Page} from './types';
+
+export const PAGES: Page[] = [
+  {name: 'About', id: 'about'},
+  {name: 'Contact', id: 'contact'},
+  {name: 'Services', id: 'services'},
+  {name: 'Blog', id: 'blog'},
+  {name: 'Portfolio', id: 'portfolio'},
+  {name: 'Admin', id: 'admin'},
+];
