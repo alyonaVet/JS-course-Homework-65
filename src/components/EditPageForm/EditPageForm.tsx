b1a10dfd4423e3a5a0febcd083c9598984408e44
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-05 11:14:35 +0600

    create component EditPageForm;

---
+++ src/components/EditPageForm/EditPageForm.tsx
@@ -0,0 +1,43 @@
+import React, {ChangeEvent, FormEvent} from 'react';
+import {ApiPage} from '../../types';
+
+interface EditPageFormProps {
+  page: ApiPage;
+  onFieldChange: (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => void;
+  onFormSubmit: (event: FormEvent<HTMLFormElement>) => void;
+}
+
+const EditPageForm: React.FC<EditPageFormProps> = ({page, onFormSubmit, onFieldChange}) => {
+  return (
+    <form onSubmit={onFormSubmit}>
+      <div className="form-group">
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          required
+          className="form-control w-50 mb-3"
+          onChange={onFieldChange}
+          value={page.title}
+        />
+      </div>
+      <div className="form-group mt-3">
+        <label htmlFor="content">Content</label>
+        <textarea
+          name="content"
+          id="content"
+          required
+          className="form-control"
+          onChange={onFieldChange}
+          value={page.content}
+        />
+      </div>
+      <button type="submit" className="btn btn-primary mt-4">
+        Save
+      </button>
+    </form>
+  );
+};
+
+export default EditPageForm;
